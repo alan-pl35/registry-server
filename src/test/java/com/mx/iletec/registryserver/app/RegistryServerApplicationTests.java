cab3e08c454f0b412deb1202015cc8b3bf82cbc3
alan-pl35 <alan.puga35@outlook.com>
2024-04-10 19:03:40 -0600

    se agrega proyecto a repostorio

---
+++ src/test/java/com/mx/iletec/registryserver/app/RegistryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mx.iletec.registryserver.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
